aa3139e6f59eff4519e0e96a9882fbfaa3c3309e
Hoàng Hiệp <hoanghiepg6@MacBook-Air-cua-Hoang-Hiep.local>
2024-07-30 23:51:20 +0700

    Initial commit

---
+++ Tk.py
@@ -0,0 +1,8 @@
+from tkinter import*
+window = Tk()
+window.geometry("420x420")
+window.title("Tic Tac Toe")
+icon = PhotoImage(file = "")
+window.iconphoto(True,icon)
+
+window.mainloop()
---
+++ tessttt.py
@@ -0,0 +1,6 @@
+import numpy as np
+import pandas as pd
+a = np.array([[6],[0],[3],[6]])
+b = np.array([[4,2,1]])
+c = np.matmul(a,b)
+print(c)
